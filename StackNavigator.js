cd019eac73f3126c0c7acdbb123a7e77e8a3545b
Sumit <jagtasumit668@gmail.com>
2024-02-07 01:58:01 +0530

    login and register

--- StackNavigator.js
+++ StackNavigator.js
@@ -6,6 +6,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MovieScreen from "./screens/MovieScreen";
 import TheatreScreen from "./screens/TheatreScreen";
 import TicketScreen from "./screens/TicketScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
 export default function StackNavigator() {
   const Stack = createNativeStackNavigator();
@@ -13,6 +15,16 @@ export default function StackNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen
           name="Home"
           component={HomeScreen}
---
+++ screens/RegisterScreen.js
@@ -0,0 +1,203 @@
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+} from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import {
+  AntDesign,
+  MaterialIcons,
+  EvilIcons,
+  Entypo,
+} from "@expo/vector-icons";
+export default function LoginScreen() {
+  const navigation = useNavigation();
+  return (
+    <SafeAreaView style={{ backgroundColor: "white", height: "100%" }}>
+      <View>
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            height: 60,
+          }}
+        >
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+            Welcome to{" "}
+            <Text
+              style={{ fontSize: 20, fontWeight: "bold", color: "#4b0082" }}
+            >
+              CinemaHub
+            </Text>
+          </Text>
+        </View>
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            style={{
+              width: 200,
+              height: 200,
+            }}
+            source={require("../assets/programmer-gif.gif")}
+          />
+        </View>
+        <View
+          style={{
+            height: 600,
+            backgroundColor: "#FFD580",
+            margin: 10,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            alignItems: "center",
+          }}
+        >
+          <View style={{ marginTop: 50 }}>
+            <View style={{ flexDirection: "row" }}>
+              <View style={{ justifyContent: "center" }}>
+                <AntDesign name="user" size={25} color="black" />
+              </View>
+              <TextInput
+                style={{
+                  width: 300,
+                  height: 40,
+                  borderWidth: 0.4,
+                  padding: 10,
+                  borderColor: "black",
+                  marginLeft: 10,
+                  alignContent: "center",
+                  borderRadius: 6,
+                }}
+              />
+            </View>
+            <View style={{ flexDirection: "row", marginTop: 20 }}>
+              <View style={{ justifyContent: "center" }}>
+                <MaterialIcons name="password" size={25} color="black" />
+              </View>
+              <TextInput
+                style={{
+                  width: 300,
+                  height: 40,
+                  borderWidth: 0.4,
+                  padding: 10,
+                  borderColor: "black",
+                  marginLeft: 10,
+                  alignContent: "center",
+                  borderRadius: 6,
+                }}
+              />
+            </View>
+            <View style={{ flexDirection: "row", marginTop: 20 }}>
+              <View style={{ justifyContent: "center" }}>
+                <MaterialIcons name="password" size={25} color="black" />
+              </View>
+              <TextInput
+                style={{
+                  width: 300,
+                  height: 40,
+                  borderWidth: 0.4,
+                  padding: 10,
+                  borderColor: "black",
+                  marginLeft: 10,
+                  alignContent: "center",
+                  borderRadius: 6,
+                }}
+              />
+            </View>
+            <View style={{ alignItems: "center", marginTop: 20 }}>
+              <Pressable
+                onPress={() => navigation.navigate("Home")}
+                style={{
+                  borderWidth: 0.3,
+                  borderColor: "black",
+                  margin: 10,
+                  padding: 10,
+                  width: 140,
+                  backgroundColor: "#4b0082",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  borderRadius: 6,
+                }}
+              >
+                <Text
+                  style={{ fontSize: 18, fontWeight: "bold", color: "white" }}
+                >
+                  Register
+                </Text>
+              </Pressable>
+            </View>
+            <Text
+              style={{
+                borderColor: "black",
+                borderStyle: "dotted",
+                borderWidth: 0.5,
+                margin: 10,
+                height: 1,
+              }}
+            />
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-around",
+                marginTop: 10,
+              }}
+            >
+              <View
+                style={{
+                  borderColor: "black",
+                  borderWidth: 0.7,
+                  margin: 10,
+                  padding: 10,
+                  borderRadius: 10,
+                }}
+              >
+                <AntDesign name="googleplus" size={24} color="black" />
+              </View>
+              <View
+                style={{
+                  borderColor: "black",
+                  borderWidth: 0.7,
+                  margin: 10,
+                  padding: 10,
+                  borderRadius: 10,
+                }}
+              >
+                <EvilIcons name="sc-facebook" size={24} color="black" />
+              </View>
+              <View
+                style={{
+                  borderColor: "black",
+                  borderWidth: 0.7,
+                  margin: 10,
+                  padding: 10,
+                  borderRadius: 10,
+                }}
+              >
+                <Entypo name="instagram" size={24} color="black" />
+              </View>
+            </View>
+          </View>
+          <Text style={{ fontSize: 16, marginTop: 15 }}>
+            Already have account?{" "}
+            <Text
+              style={{ color: "#4b0082", fontSize: 18 }}
+              onPress={() => navigation.navigate("Login")}
+            >
+              Login
+            </Text>
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
