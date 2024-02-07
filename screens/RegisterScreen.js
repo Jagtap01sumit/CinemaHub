c1ce697dd22881c77b5e1a3299833fad1f646707
Sumit <jagtasumit668@gmail.com>
2024-02-07 21:20:27 +0530

    register the user

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -9,14 +9,50 @@ import {
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
+import { useForm, Controller, FormProvider } from "react-hook-form";
 import {
   AntDesign,
   MaterialIcons,
   EvilIcons,
   Entypo,
 } from "@expo/vector-icons";
+
 export default function LoginScreen() {
   const navigation = useNavigation();
+  const methods = useForm();
+
+  const { control, handleSubmit, formState, setError } = methods;
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await fetch("http://192.168.0.103:8000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      const responseData = await response.json();
+
+      if (response.ok) {
+        methods.reset();
+        console.log("Registration successful");
+        navigation.navigate("Home");
+      } else {
+        setError("message", {
+          type: "manual",
+          message: responseData.error || "Unknown error",
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const validatePasswordMatch = (value, fieldValues) => {
+    return value === fieldValues.password || "Passwords do not match";
+  };
+
   return (
     <SafeAreaView style={{ backgroundColor: "white", height: "100%" }}>
       <View>
@@ -60,131 +96,197 @@ export default function LoginScreen() {
             alignItems: "center",
           }}
         >
-          <View style={{ marginTop: 50 }}>
-            <View style={{ flexDirection: "row" }}>
-              <View style={{ justifyContent: "center" }}>
-                <AntDesign name="user" size={25} color="black" />
+          <FormProvider {...methods}>
+            <View style={{ marginTop: 50 }}>
+              <View style={{ flexDirection: "row" }}>
+                <View style={{ justifyContent: "center" }}>
+                  <AntDesign name="user" size={25} color="black" />
+                </View>
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      style={{
+                        width: 300,
+                        height: 40,
+                        borderWidth: 0.4,
+                        padding: 10,
+                        borderColor: "black",
+                        marginLeft: 10,
+                        alignContent: "center",
+                        borderRadius: 6,
+                      }}
+                      onBlur={onBlur}
+                      onChangeText={(value) => onChange(value)}
+                      value={value}
+                      placeholder="Email"
+                    />
+                  )}
+                  name="email"
+                  rules={{
+                    required: "Email is required",
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                      message: "Invalid email address",
+                    },
+                  }}
+                />
               </View>
-              <TextInput
-                style={{
-                  width: 300,
-                  height: 40,
-                  borderWidth: 0.4,
-                  padding: 10,
-                  borderColor: "black",
-                  marginLeft: 10,
-                  alignContent: "center",
-                  borderRadius: 6,
-                }}
-              />
-            </View>
-            <View style={{ flexDirection: "row", marginTop: 20 }}>
-              <View style={{ justifyContent: "center" }}>
-                <MaterialIcons name="password" size={25} color="black" />
-              </View>
-              <TextInput
-                style={{
-                  width: 300,
-                  height: 40,
-                  borderWidth: 0.4,
-                  padding: 10,
-                  borderColor: "black",
-                  marginLeft: 10,
-                  alignContent: "center",
-                  borderRadius: 6,
-                }}
-              />
-            </View>
-            <View style={{ flexDirection: "row", marginTop: 20 }}>
-              <View style={{ justifyContent: "center" }}>
-                <MaterialIcons name="password" size={25} color="black" />
-              </View>
-              <TextInput
-                style={{
-                  width: 300,
-                  height: 40,
-                  borderWidth: 0.4,
-                  padding: 10,
-                  borderColor: "black",
-                  marginLeft: 10,
-                  alignContent: "center",
-                  borderRadius: 6,
-                }}
-              />
-            </View>
-            <View style={{ alignItems: "center", marginTop: 20 }}>
-              <Pressable
-                onPress={() => navigation.navigate("Home")}
-                style={{
-                  borderWidth: 0.3,
-                  borderColor: "black",
-                  margin: 10,
-                  padding: 10,
-                  width: 140,
-                  backgroundColor: "#4b0082",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  borderRadius: 6,
-                }}
-              >
-                <Text
-                  style={{ fontSize: 18, fontWeight: "bold", color: "white" }}
-                >
-                  Register
+              {formState.errors.email && (
+                <Text style={{ color: "red", marginLeft: 37 }}>
+                  {formState.errors.email.message}
                 </Text>
-              </Pressable>
+              )}
+
+              <View style={{ flexDirection: "row", marginTop: 20 }}>
+                <View style={{ justifyContent: "center" }}>
+                  <MaterialIcons name="password" size={25} color="black" />
+                </View>
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      style={{
+                        width: 300,
+                        height: 40,
+                        borderWidth: 0.4,
+                        padding: 10,
+                        borderColor: "black",
+                        marginLeft: 10,
+                        alignContent: "center",
+                        borderRadius: 6,
+                      }}
+                      onBlur={onBlur}
+                      onChangeText={(value) => onChange(value)}
+                      value={value}
+                      secureTextEntry
+                      placeholder="Password"
+                    />
+                  )}
+                  name="password"
+                  rules={{ required: "Password is required" }}
+                />
+              </View>
+              {formState.errors.password && (
+                <Text style={{ color: "red", marginLeft: 37 }}>
+                  {formState.errors.password.message}
+                </Text>
+              )}
+              <View style={{ flexDirection: "row", marginTop: 20 }}>
+                <View style={{ justifyContent: "center" }}>
+                  <MaterialIcons name="password" size={25} color="black" />
+                </View>
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      style={{
+                        width: 300,
+                        height: 40,
+                        borderWidth: 0.4,
+                        padding: 10,
+                        borderColor: "black",
+                        marginLeft: 10,
+                        alignContent: "center",
+                        borderRadius: 6,
+                      }}
+                      onBlur={onBlur}
+                      onChangeText={(value) => onChange(value)}
+                      value={value}
+                      secureTextEntry
+                      placeholder="Confirm Password"
+                    />
+                  )}
+                  name="confirmPass"
+                  rules={{
+                    required: "Password is required",
+                    validate: (value) =>
+                      validatePasswordMatch(value, methods.getValues()),
+                  }}
+                />
+              </View>
+              {formState.errors.confirmPass && (
+                <Text style={{ color: "red", marginLeft: 37 }}>
+                  {formState.errors.confirmPass.message}
+                </Text>
+              )}
+              <Text style={{ color: "red", marginLeft: 37 }}>
+                {formState.errors.message?.message}
+              </Text>
+              <View style={{ alignItems: "center", marginTop: 20 }}>
+                <Pressable
+                  style={{
+                    borderWidth: 0.3,
+                    borderColor: "black",
+                    margin: 10,
+                    padding: 10,
+                    width: 140,
+                    backgroundColor: "#4b0082",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderRadius: 6,
+                  }}
+                  onPress={handleSubmit(onSubmit)}
+                >
+                  <Text
+                    style={{ fontSize: 18, fontWeight: "bold", color: "white" }}
+                  >
+                    Register
+                  </Text>
+                </Pressable>
+              </View>
+              <Text
+                style={{
+                  borderColor: "black",
+                  borderStyle: "dotted",
+                  borderWidth: 0.5,
+                  margin: 10,
+                  height: 1,
+                }}
+              />
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-around",
+                  marginTop: 10,
+                }}
+              >
+                <View
+                  style={{
+                    borderColor: "black",
+                    borderWidth: 0.7,
+                    margin: 10,
+                    padding: 10,
+                    borderRadius: 10,
+                  }}
+                >
+                  <AntDesign name="googleplus" size={24} color="black" />
+                </View>
+                <View
+                  style={{
+                    borderColor: "black",
+                    borderWidth: 0.7,
+                    margin: 10,
+                    padding: 10,
+                    borderRadius: 10,
+                  }}
+                >
+                  <EvilIcons name="sc-facebook" size={24} color="black" />
+                </View>
+                <View
+                  style={{
+                    borderColor: "black",
+                    borderWidth: 0.7,
+                    margin: 10,
+                    padding: 10,
+                    borderRadius: 10,
+                  }}
+                >
+                  <Entypo name="instagram" size={24} color="black" />
+                </View>
+              </View>
             </View>
-            <Text
-              style={{
-                borderColor: "black",
-                borderStyle: "dotted",
-                borderWidth: 0.5,
-                margin: 10,
-                height: 1,
-              }}
-            />
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-around",
-                marginTop: 10,
-              }}
-            >
-              <View
-                style={{
-                  borderColor: "black",
-                  borderWidth: 0.7,
-                  margin: 10,
-                  padding: 10,
-                  borderRadius: 10,
-                }}
-              >
-                <AntDesign name="googleplus" size={24} color="black" />
-              </View>
-              <View
-                style={{
-                  borderColor: "black",
-                  borderWidth: 0.7,
-                  margin: 10,
-                  padding: 10,
-                  borderRadius: 10,
-                }}
-              >
-                <EvilIcons name="sc-facebook" size={24} color="black" />
-              </View>
-              <View
-                style={{
-                  borderColor: "black",
-                  borderWidth: 0.7,
-                  margin: 10,
-                  padding: 10,
-                  borderRadius: 10,
-                }}
-              >
-                <Entypo name="instagram" size={24} color="black" />
-              </View>
-            </View>
-          </View>
+          </FormProvider>
           <Text style={{ fontSize: 16, marginTop: 15 }}>
             Already have account?{" "}
             <Text
